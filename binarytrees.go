ba9ac0a1a2242ffb9a6dbd62de5714fb17d9f828
Phuoc Thanh <nguyenphuocthanhtn2015@gmail.com>
2020-05-12 15:40:26 +0700

    Update binarytrees.go
    Make solutions more readable

--- binarytrees.go
+++ binarytrees.go
@@ -1,67 +1,51 @@
 package main
 
-import (
-	"fmt"
-
-	"golang.org/x/tour/tree"
-)
-
-func walkImpl(t *tree.Tree, ch, quit chan int) {
-	if t == nil {
-		return
-	}
-	walkImpl(t.Left, ch, quit)
-	select {
-	case ch <- t.Value:
-		// Value successfully sent.
-	case <-quit:
-		return
-	}
-	walkImpl(t.Right, ch, quit)
-}
+import "golang.org/x/tour/tree"
+import "fmt"
 
 // Walk walks the tree t sending all values
 // from the tree to the channel ch.
-func Walk(t *tree.Tree, ch, quit chan int) {
-	walkImpl(t, ch, quit)
+func Walk(t *tree.Tree, ch chan int) {
+	var walker func(t *tree.Tree)
+	walker = func(t *tree.Tree) {
+		if t == nil {
+			return
+		}
+		
+		walker(t.Left)
+		ch <- t.Value
+		walker(t.Right)
+	}	
+	
+	walker(t)
 	close(ch)
+	
 }
 
 // Same determines whether the trees
 // t1 and t2 contain the same values.
 func Same(t1, t2 *tree.Tree) bool {
-	w1, w2 := make(chan int), make(chan int)
-	quit := make(chan int)
-	defer close(quit)
-
-	go Walk(t1, w1, quit)
-	go Walk(t2, w2, quit)
-
+	ch1, ch2 := make(chan int), make(chan int)
+	
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+	
 	for {
-		v1, ok1 := <-w1
-		v2, ok2 := <-w2
-		if !ok1 || !ok2 {
-			return ok1 == ok2
-		}
-		if v1 != v2 {
+		v1, ok1 := <- ch1
+		v2, ok2 := <- ch2
+		if v1 != v2 || ok1 != ok2 {
 			return false
 		}
+		
+		if !ok1 {
+			break
+		}
+		
 	}
+	return true
 }
 
 func main() {
-	fmt.Print("tree.New(1) == tree.New(1): ")
-	if Same(tree.New(1), tree.New(1)) {
-		fmt.Println("PASSED")
-	} else {
-		fmt.Println("FAILED")
-	}
-
-	fmt.Print("tree.New(1) != tree.New(2): ")
-	if !Same(tree.New(1), tree.New(2)) {
-		fmt.Println("PASSED")
-	} else {
-		fmt.Println("FAILED")
-	}
+	fmt.Println("1 and 1 same: ", Same(tree.New(1), tree.New(1)))
+    fmt.Println("1 and 2 same: ", Same(tree.New(1), tree.New(2)))
 }
-
